0fce67b2ff17c5997f77094e4038da9240a560c5
diarrabell <36339346+diarrabell@users.noreply.github.com>
2023-08-02 21:37:41 -0400

    debugging

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 This CLI tool allows you to test the sentiment analysis API with your own data. 
 It takes in a movie review and returns the predicted sentiment (positive or negative).
 """
-import requests
 import click
+import requests
 
 @click.command()
 @click.option('--text', help='text to be classified')
@@ -14,15 +14,13 @@ def predict(text): # pylint: disable=no-value-for-parameter
     headers = {
     'Content-Type': 'text/plain'
     }
-    response = requests.request("POST", url, headers=headers, data=payload, timeout=10)  
-    return response.text # returns numerical value of prediction. 0 is negative, 1 is positive.
-
-def main(text):
-    response = predict(text)
-    if "1" in response:
+    response = requests.request("POST", url, headers=headers, data=payload, timeout=10)
+    click.echo(response.text)  
+    if "1" in response.text:
         click.echo("Sentiment prediction: positive")
     else:
         click.echo("Sentiment prediction: negative") 
+    return response.text # returns numerical value of prediction. 0 is negative, 1 is positive.
 
-if __name__ == "__main__":
-    main() # pylint: disable=no-value-for-parameter
+if __name__ == '__main__':
+    predict() # pylint: disable=no-value-for-parameter
--- test_main.py
+++ test_main.py
@@ -1,11 +1,16 @@
 import pytest
+import click
 from click.testing import CliRunner
 from main import predict
 
 def test():
-    results = predict("this is a good review!")
-    print(results)
-    assert int(results[-2]) in [0, 1]
+    runner = CliRunner()
+    text = "This is a good review"
+    results = runner.invoke(predict, ['--text', text])
+    results = results.output.split(":")
+    results = results[1].split("}")
+    print(results[0])
+    assert int(results[0]) in [0, 1]
 
 if __name__ == "__main__":
     test()
